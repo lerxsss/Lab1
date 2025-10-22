a8e46b8ad9a9d31595cc7d59cd3fb9f66ad099f0
Valeria Bielousova <lerusia136@gmail.com>
2025-10-23 00:13:36 +0300

    Lab 8: [Add] employee card to about.html; fetch() for get data from JSONPlaceholder

--- src/main.js
+++ src/main.js
@@ -2,4 +2,19 @@ import '@/styles/tw.css';
 import '@/styles/main.scss';
 
 import { initThemeToggle } from '/src/js/common/theme.js';
+import { initAbout } from '/src/js/app/aboutPage.js';
+
 initThemeToggle();
+
+let cleanupFunction = () => {};
+
+initAbout()
+    .then(abortFn => {
+        cleanupFunction = abortFn;
+        console.log('Ініціалізація About Page завершена. Функція скасування збережена.');
+    })
+    .catch(error => {
+        console.error('Помилка при ініціалізації About Page:', error);
+    });
+
+// cleanupFunction();
