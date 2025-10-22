114b19c6e8a33c098eaefea57420058222d545a2
Valeria Bielousova <lerusia136@gmail.com>
2025-10-23 00:40:38 +0300

    Lab 8: [Add] Code Splitting

--- src/main.js
+++ src/main.js
@@ -7,14 +7,21 @@ import { initAbout } from '/src/js/app/aboutPage.js';
 initThemeToggle();
 
 let cleanupFunction = () => {};
+const page = document.body.dataset.page;
 
-initAbout()
-    .then(abortFn => {
-        cleanupFunction = abortFn;
-        console.log('Ініціалізація About Page завершена. Функція скасування збережена.');
-    })
-    .catch(error => {
-        console.error('Помилка при ініціалізації About Page:', error);
+const routes = {
+    about:    () => import('./js/app/aboutPage.js').then(m => m.initAbout()
+        .then(abortFn => {
+            cleanupFunction = abortFn;
+            console.log('Ініціалізація About Page завершена. Функція скасування збережена.');
+        })),
+};
+
+if (routes[page]) {
+    routes[page]().catch(err => {
+        console.error('Помилка ініціалізації сторінки:', err);
     });
+}
+
 
 // cleanupFunction();
